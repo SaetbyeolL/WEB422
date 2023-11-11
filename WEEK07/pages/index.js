a387cf140982a943555084e683c5dd4cb82565a8
Saetbyeol Lim <saetbyeol701@gmail.com>
2023-11-11 02:27:46 -0500

    WEEK07: Managing Application State

---
+++ WEEK07/pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <h3>Shopping Cart Demo</h3>
+      <p>This site demonstrates application state management in React</p>
+    </>
+  )
+}
+<<<<<<< HEAD
+  
+=======
+ 
+>>>>>>> d8e2768 (WEEK07)
